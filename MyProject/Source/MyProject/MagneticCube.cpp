7bb065d3694c4ca3a240f33bea72d1c7ca9b739d
CryingOnion15 <yconnor13@gmail.com>
2019-10-12 00:49:35 -0500

    Cube Mesh and Box Component Set up

--- MyProject/Source/MyProject/MagneticCube.cpp
+++ MyProject/Source/MyProject/MagneticCube.cpp
@@ -2,6 +2,9 @@
 
 
 #include "MagneticCube.h"
+#include "Components/BoxComponent.h"
+#include "Engine/StaticMesh.h"
+#include "ConstructorHelpers.h"
 
 // Sets default values
 AMagneticCube::AMagneticCube()
@@ -9,6 +12,20 @@ AMagneticCube::AMagneticCube()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	UPROPERTY(EditDefaultsOnly)
+	UStaticMeshComponent* CubeMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	UBoxComponent* Cube = CreateDefaultSubobject<UBoxComponent>(TEXT("Root Cube"));
+
+	RootComponent = Cube;
+	CubeMesh->SetupAttachment(RootComponent);
+
+	
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>MeshAsset(TEXT("StaticMesh'/Game/StarterContent/Shapes/Shape_Cube.Shape_Cube'"));
+	UStaticMesh* Asset = MeshAsset.Object;
+
+	CubeMesh->SetStaticMesh(Asset);
+	CubeMesh->SetRelativeLocation(FVector(0.0f, 0.0f, -40.0f));
+	CubeMesh->SetWorldScale3D(FVector(0.8f));
 }
 
 // Called when the game starts or when spawned
